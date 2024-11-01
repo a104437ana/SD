febaee06c92d3f4980115d875dff75974e0ab619
a104437ana <a104437@alunos.uminho.pt>
2024-11-01 18:00:56 +0000

    Adicionadas operações put e get

--- trabalho-pratico/ArmazemDadosPartilhados.java
+++ trabalho-pratico/ArmazemDadosPartilhados.java
@@ -6,11 +6,14 @@ class ArmazemDadosPartilhados {
     private static class Dado {
         private byte[] value;
         Lock l = new ReentrantLock();
-        Dado (byte[] value) {
+        public Dado(byte[] value) {
             this.value = value;
         }
-        byte[] value() {
+        public byte[] getValue() {
             return this.value;
+        }
+        public void setValue(byte[] value) {
+            this.value = value;
         }
     }
 
@@ -18,27 +21,53 @@ class ArmazemDadosPartilhados {
     Lock l = new ReentrantLock();
 
     public void put(String key, byte[] value) {
-        
-    }
-
-    public byte[] get (String key) {
-        Dado dado = map.get(key);
-        if (dado == null) return null;
-        else {
-            dado.l.lock();
-            try {
-                return dado.value();
-            } finally {
-                dado.l.unlock();
+        Dado dado;
+        l.lock();
+        try {
+            dado = map.get(key);
+            if (dado == null) {
+                dado = new Dado(value);
+                map.put(key,value);
+                return;
             }
+            dado.l.lock();
+        } finally {
+            l.unlock();
+        }
+        try {
+            dado.setValue(value);
+        } finally {
+            dado.l.unlock();
         }
     }
 
-    public void multiPut (Map<String,byte[]> pairs) {
+    public byte[] get(String key) {
+        Dado dado;
+        l.lock();
+        try {
+            dado = map.get(key);
+            if (dado == null) return null;
+            dado.l.lock();
+        } finally {
+            l.unlock();
+        }
+        try {
+            return dado.getValue();
+        } finally {
+            dado.l.unlock();
+        }
+    }
+
+    public void multiPut(Map<String,byte[]> pairs) {
 
     }
 
-    public Map<String,byte[]> multiGet (Set<String> keys) {
+    public Map<String,byte[]> multiGet(Set<String> keys) {
+        Map<String,byte[]> values = new HashMap<String,byte[]>();
+
+    }
+
+    public byte[] getWhen(String key, String keyCond, byte[] valueCond) {
 
     }
 }
