1fe7e5fc1e79cbf157b673e52669557f7f4f7869
rafael-vilasboas <rafaelvilasboas0102@gmail.com>
2024-12-19 22:04:03 +0000

    Adicionados alguns testes de desempenho e exportação dos resultados para csv

--- trabalho-pratico/CsvExport.java
+++ trabalho-pratico/CsvExport.java
@@ -1,2 +1,63 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CsvExport {
+
+    private static final String SEPARATOR = ";";
+    private static final String NEW_LINE = "\n";
+
+    public void exportDataCsv(List<String[]> data, String directory, String name) {
+        File dir = new File(directory);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+        int id = 0;
+        String fileName = name + id + ".csv";
+        File file = new File(dir, fileName);
+        while (file.exists()) {
+            id++;
+            fileName = name + id + ".csv";
+            file = new File(dir, fileName);
+        }
+
+        try {
+            BufferedWriter fw = new BufferedWriter(new FileWriter(file));
+
+            for (String[] line : data) {
+                StringBuilder sb = new StringBuilder();
+                int i;
+                for (i = 0; i < line.length - 1; i++) {
+                    String field = line[i];
+                    sb.append(field).append(SEPARATOR);
+                }
+                sb.append(line[i]);
+                sb.append(NEW_LINE);
+                String convertedLine = sb.toString();
+                fw.write(convertedLine);
+            }
+
+            fw.flush();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] header = {"op", "time", "type"};
+        String[] line1 = {"1", "20", "0"};
+        String[] line2 = {"2", "50", "1"};
+
+        List<String[]> list = new ArrayList<>();
+        list.add(header);
+        list.add(line1);
+        list.add(line2);
+
+        CsvExport csvExport = new CsvExport();
+        csvExport.exportDataCsv(list, "tmp", "test");
+    }
 }
--- trabalho-pratico/TestClientApp.java
+++ trabalho-pratico/TestClientApp.java
@@ -1,33 +1,128 @@
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class TestClientApp {
     
     private static final int VALUE_SIZE = 1024;
+    private static final String DIRECTORY = "tmp";
+    private static final String NAME = "test";
 
     private float putWorkload(long ops) {
         Client client = new ClientSingleThread();
         Random rand = new Random();
         byte[] value = new byte[VALUE_SIZE];
+        List<String[]> data = new ArrayList<String[]>();
+        String[] header = new String[] {"op", "time", "key"};
+        data.add(header);
         Timestamp start = new Timestamp(System.currentTimeMillis());
 
-        for (int i = 0; i < ops; i++) {
+        for (long i = 0; i < ops; i++) {
+            long time = System.currentTimeMillis();
             String key = Long.toString(rand.nextLong(ops));
             client.put(key, value);
+            time = System.currentTimeMillis() - time;
+            String[] line = new String[] {Long.toString(i), Long.toString(time), key};
+            data.add(line);
         }
 
         Timestamp end = new Timestamp(System.currentTimeMillis());
+        CsvExport csvExport = new CsvExport();
+        csvExport.exportDataCsv(data, DIRECTORY, NAME);
         return end.getTime() - start.getTime();
     }
 
     private float getWorkload(long ops) {
-        return 0;
+        Client client = new ClientSingleThread();
+        Random rand = new Random();
+        byte[] value = new byte[VALUE_SIZE];
+        List<String[]> data = new ArrayList<String[]>();
+        String[] header = new String[] {"op", "time", "key"};
+        data.add(header);
+
+        // Inicialização dos dados no servidor
+        for (long i = 0; i < ops; i++) {
+            long time = System.currentTimeMillis();
+            String key = Long.toString(i);
+            client.put(key, value);
+            time = System.currentTimeMillis() - time;
+            String[] line = new String[] {Long.toString(i), Long.toString(time), key};
+            data.add(line);
+        }
+
+        Timestamp start = new Timestamp(System.currentTimeMillis());
+
+        // Falta 99% dos acessos em 1% do dataset
+        for (long i = 0; i < ops; i++) {
+            String key = Long.toString(rand.nextLong(ops));
+            value = client.get(key);
+        }
+
+        Timestamp end = new Timestamp(System.currentTimeMillis());
+        CsvExport csvExport = new CsvExport();
+        csvExport.exportDataCsv(data, DIRECTORY, NAME);
+        return end.getTime() - start.getTime();
     }
 
     private float putGetWorkload(long ops, int ratio) {
-        return 0;
+        Client client = new ClientSingleThread();
+        Random rand = new Random();
+        byte[] value = new byte[VALUE_SIZE];
+        List<String[]> data = new ArrayList<String[]>();
+        String[] header = new String[] {"op", "time", "key", "type"};
+        data.add(header);
+
+        // Inicialização dos dados no servidor
+        for (long i = 0; i < ops; i++) {
+            String key = Long.toString(i);
+            client.put(key, value);
+        }
+
+        Timestamp start = new Timestamp(System.currentTimeMillis());
+
+        // Falta 99% dos acessos em 1% do dataset no get
+        for (long i = 0; i < ops; i++) {
+            long time = System.currentTimeMillis();
+            int r = rand.nextInt(100);
+            String key;
+            boolean isPut = r < ratio ? true : false;
+            // Put
+            if (isPut) {
+                key = Long.toString(rand.nextLong(ops));
+                client.put(key, value);
+            }
+            // Get
+            else {
+                key = Long.toString(rand.nextLong(ops));
+                value = client.get(key);
+            }
+            time = System.currentTimeMillis() - time;
+            String[] line = new String[] {Long.toString(i), Long.toString(time), key, Boolean.toString(isPut)};
+            data.add(line);
+        }
+
+        Timestamp end = new Timestamp(System.currentTimeMillis());
+        CsvExport csvExport = new CsvExport();
+        csvExport.exportDataCsv(data, DIRECTORY, NAME);
+        return end.getTime() - start.getTime();
     }
 
     public static void main(String[] args) {
+        TestClientApp testClient = new TestClientApp();
+        if (args.length > 1) {
+            String test = args[0];
+            long ops = Long.parseLong(args[1]);
+            float time;
+            if (test.equals("get")) {
+                time = testClient.putWorkload(ops);
+            }
+            else if (test.equals("put")) {
+                time = testClient.getWorkload(ops);
+            }
+            else if (test.equals("getPut") && args.length == 3) {
+                time = testClient.putGetWorkload(ops, Integer.parseInt(args[3]));
+            }
+        }
     }
 }
