c61abe6e25687012b55f74c2947a990c0c1a81f0
a104437ana <a104437@alunos.uminho.pt>
2024-12-19 20:32:25 +0000

    Adicionada operação getWhen e melhoria das outras operações

--- trabalho-pratico/ArmazemDadosPartilhados.java
+++ trabalho-pratico/ArmazemDadosPartilhados.java
@@ -1,11 +1,19 @@
-import java.util.*;
-import java.util.concurrent.locks.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.locks.Condition;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Map;
+import java.util.TreeMap;
 
 class ArmazemDadosPartilhados {
 
     private static class Dado {
         private byte[] value;
         Lock l = new ReentrantLock();
+        Condition c = l.newCondition();
         public Dado(byte[] value) {
             this.value = value;
         }
@@ -23,6 +31,7 @@ class ArmazemDadosPartilhados {
     //Se tentar usar null como chave, teremos NullPointerException
     private Map<String,Dado> map = new TreeMap<String,Dado>();
     Lock l = new ReentrantLock();
+    Condition c = l.newCondition();
 
     //pré-condição: key e value não nulos
     public void put(String key, byte[] value) {
@@ -33,6 +42,7 @@ class ArmazemDadosPartilhados {
             if (dado == null) {
                 dado = new Dado(value);
                 map.put(key,dado);
+                c.signalAll();
                 return;
             }
             dado.l.lock();
@@ -41,6 +51,7 @@ class ArmazemDadosPartilhados {
         }
         try {
             dado.setValue(value);
+            dado.c.signalAll();
         } finally {
             dado.l.unlock();
         }
@@ -48,6 +59,7 @@ class ArmazemDadosPartilhados {
 
     //pré-condição: key não nula
     public byte[] get(String key) {
+        byte [] value;
         Dado dado;
         l.lock();
         try {
@@ -58,10 +70,11 @@ class ArmazemDadosPartilhados {
             l.unlock();
         }
         try {
-            return dado.getValue();
+            value = dado.getValue();
         } finally {
             dado.l.unlock();
         }
+        return value;
     }
     //se a chave não existir retornamos null,
     //logo os valores não podem ser nulos
@@ -69,69 +82,94 @@ class ArmazemDadosPartilhados {
     //do facto de não existir chave
     //pois das duas maneiras iriamos retornar null
 
-    //pré-condição: não tem chaves ou valores nulos
+    //pré-condição: não tem chaves nem valores nulos
     public void multiPut(Map<String,byte[]> pairs) {
-        TreeMap<String,byte[]> pairs2 = new TreeMap<String,byte[]>(pairs);
-        Dado[] dado = new Dado[pairs2.size()];
+        Map<String,byte[]> orderedPairs = new TreeMap<String,byte[]>(pairs);
+        List<Dado> dados = new ArrayList<Dado>();
+        List<byte[]> values = new ArrayList<byte[]>();
         l.lock();
-        int i;
+        Dado dado;
+        String key;
+        byte[] value;
         try {
-            i = 0;
-            for (Map.Entry<String,byte[]> pair : pairs2.entrySet()) {
-                String key = pair.getKey();
-                dado[i] = map.get(key);
-                if (dado[i] == null) {
-                    byte[] value = pair.getValue();
-                    Dado d = new Dado(value);
-                    map.put(key,d);
+            for (Map.Entry<String,byte[]> pair : orderedPairs.entrySet()) {
+                key = pair.getKey();
+                value = pair.getValue();
+                dado = map.get(key);
+                if (dado == null) {
+                    dado = new Dado(value);
+                    map.put(key,dado);
+                    c.signalAll();
                 }
                 else {
-                    dado[i].l.lock();
+                    dados.add(dado);
+                    values.add(value);
+                    dado.l.lock();
                 }
-                i++;
             }
         } finally {
             l.unlock();
         }
-        i = 0;
-        for (byte[] value : pairs2.values()) {
-            if (dado[i] != null) {
-                dado[i].setValue(value);
-                dado[i].l.unlock();
-            }
-            i++;
+        for (int i = 0; i < dados.size(); i++) {
+            dado = dados.get(i);
+            value = values.get(i);
+            dado.setValue(value);
+            dado.c.signalAll();
+            dado.l.unlock();
         }
     }
 
     //pré-condição: não tem chaves nulas
     public Map<String,byte[]> multiGet(Set<String> keys) {
         Map<String,byte[]> pairs = new TreeMap<String,byte[]>();
-        TreeSet<String> orderedKeys = new TreeSet<String>(keys);
-        Dado[] dado = new Dado[orderedKeys.size()];
+        Set<String> orderedKeys = new TreeSet<String>(keys);
+        List<Dado> dados = new ArrayList<Dado>();
+        List<String> keysWithDados = new ArrayList<String>();
         l.lock();
+        Dado dado;
+        byte[] value;
         try {
-            for (int i = 0; i < orderedKeys.size(); i++) {
-                dado[i] = map.get(orderedKeys[i]); //orderedKeys é um treeset não um array
-                if (dado[i] != null) dado[i].l.lock();
-                //else pairs.put(orderedKeys[i],null);
+            for (String key : orderedKeys) {
+                dado = map.get(key);
+                if (dado != null) {
+                    dados.add(dado);
+                    keysWithDados.add(key);
+                    dado.l.lock();
+                }
+                //else pairs.put(key,null);
             }
         } finally {
             l.unlock();
         }
-        int i = 0;
-        byte[] value;
-        for (Dado d : dado) {
-            if (d != null) {
-                value = d.getValue();
-                pairs.put(orderedKeys[i],value);
-                d.l.unlock();
-            }
-            i++;
+        for (int i = 0; i < dados.size(); i++) {
+            dado = dados.get(i);
+            value = dado.getValue();
+            String key = keysWithDados.get(i);
+            pairs.put(key,value);
+            dado.l.unlock();
         }
         return pairs;
     }
 
-    public byte[] getWhen(String key, String keyCond, byte[] valueCond) {
-
+    public byte[] getWhen(String key, String keyCond, byte[] valueCond) throws InterruptedException {
+        Dado dado;
+        l.lock();
+        try {
+            //if (dado == null) return null;
+            while ((dado = map.get(keyCond)) == null) {
+                c.await();
+            }
+            dado.l.lock();
+        } finally {
+            l.unlock();
+        }
+        try {
+          while (dado.getValue() != valueCond) {
+            dado.c.await();
+          }
+        } finally {
+            dado.l.unlock();
+        }
+        return get(key);
     }
 }
