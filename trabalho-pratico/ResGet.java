a914c83e03a9506677c0a5d21d7dda1421312b98
a104263 <a104263@alunos.uminho.pt>
2024-12-20 21:37:15 +0000

    Corrigido resGet

--- trabalho-pratico/ResGet.java
+++ trabalho-pratico/ResGet.java
@@ -1,37 +1,27 @@
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class ResGet implements Message {
-    ArrayList<byte[]> value;    
+    byte[] value;    
     
-    public ResGet(ArrayList<byte[]> value){
+    public ResGet(byte[] value){
         this.value=value;
     }
     
     public void serialize(DataOutputStream out) throws IOException{
         try {
-            out.writeInt(this.value.size());
-            for(byte[] b : value){
-                out.writeInt(b.length);  
-                out.write(b);
-            }
+            out.writeInt(value.length);  
+            out.write(value);
         } catch (IOException e) {
         }
     }
 
     public Message deserialize(DataInputStream in)throws IOException {
         try{
-            ArrayList<byte[]> resposta= new ArrayList<>();
             int tamanho=in.readInt();
-            int i=0;
-            while(i<tamanho){
-                int tamanhoB=in.readInt();
-                byte[] b=new byte[tamanhoB];
-                in.readFully(b);
-                resposta.add(b);
-            }
+            byte[] resposta=new byte[tamanho];
+            in.readFully(resposta);
             return new ResGet(resposta);
         }catch (IOException e){
             return null;
