faa636ff36854eedaaeb0438ff065cfa782e9faa
rafael-vilasboas <rafaelvilasboas0102@gmail.com>
2024-12-21 15:33:42 +0000

    Tratamento de erros e alguns testes de mensagens

--- trabalho-pratico/TestClientApp.java
+++ trabalho-pratico/TestClientApp.java
@@ -8,8 +8,10 @@ public class TestClientApp {
     private static final int VALUE_SIZE = 1024;
     private static final String DIRECTORY = "tmp";
     private static final String NAME = "test";
+    private String fileName = "";
 
     private float putWorkload(long ops) {
+        TestClientApp.testsStartMessage();
         Client client = new ClientSingleThread();
         Random rand = new Random();
         byte[] value = new byte[VALUE_SIZE];
@@ -21,19 +23,25 @@ public class TestClientApp {
         for (long i = 0; i < ops; i++) {
             long time = System.currentTimeMillis();
             String key = Long.toString(rand.nextLong(ops));
-            client.put(key, value);
+            ///////////////////// Apenas para testar /////////////////////
+            try { Thread.sleep(rand.nextInt(50 - 20) + 20); }         ////
+            catch (InterruptedException e) { e.printStackTrace(); }   ////
+            //////////////////////////////////////////////////////////////
+//            client.put(key, value);
             time = System.currentTimeMillis() - time;
-            String[] line = new String[] {Long.toString(i), Long.toString(time), key};
+            String[] line = new String[] {Long.toString(i+1), Long.toString(time), key};
             data.add(line);
         }
 
         Timestamp end = new Timestamp(System.currentTimeMillis());
+        String testName = NAME + "Get";
         CsvExport csvExport = new CsvExport();
-        csvExport.exportDataCsv(data, DIRECTORY, NAME);
+        fileName = csvExport.exportDataCsv(data, DIRECTORY, testName);
         return end.getTime() - start.getTime();
     }
 
     private float getWorkload(long ops) {
+        TestClientApp.testsStartMessage();
         Client client = new ClientSingleThread();
         Random rand = new Random();
         byte[] value = new byte[VALUE_SIZE];
@@ -43,29 +51,35 @@ public class TestClientApp {
 
         // Inicialização dos dados no servidor
         for (long i = 0; i < ops; i++) {
-            long time = System.currentTimeMillis();
             String key = Long.toString(i);
-            client.put(key, value);
-            time = System.currentTimeMillis() - time;
-            String[] line = new String[] {Long.toString(i), Long.toString(time), key};
-            data.add(line);
+//            client.put(key, value);
         }
 
         Timestamp start = new Timestamp(System.currentTimeMillis());
 
         // Falta 99% dos acessos em 1% do dataset
         for (long i = 0; i < ops; i++) {
+            long time = System.currentTimeMillis();
             String key = Long.toString(rand.nextLong(ops));
-            value = client.get(key);
+            ///////////////////// Apenas para testar /////////////////////
+            try { Thread.sleep(rand.nextInt(50 - 20) + 20); }         ////
+            catch (InterruptedException e) { e.printStackTrace(); }   ////
+            //////////////////////////////////////////////////////////////
+//            value = client.get(key);
+            time = System.currentTimeMillis() - time;
+            String[] line = new String[] {Long.toString(i+1), Long.toString(time), key};
+            data.add(line);
         }
 
         Timestamp end = new Timestamp(System.currentTimeMillis());
+        String testName = NAME + "Put";
         CsvExport csvExport = new CsvExport();
-        csvExport.exportDataCsv(data, DIRECTORY, NAME);
+        fileName = csvExport.exportDataCsv(data, DIRECTORY, testName);
         return end.getTime() - start.getTime();
     }
 
     private float putGetWorkload(long ops, int ratio) {
+        TestClientApp.testsStartMessage();
         Client client = new ClientSingleThread();
         Random rand = new Random();
         byte[] value = new byte[VALUE_SIZE];
@@ -76,7 +90,7 @@ public class TestClientApp {
         // Inicialização dos dados no servidor
         for (long i = 0; i < ops; i++) {
             String key = Long.toString(i);
-            client.put(key, value);
+//            client.put(key, value);
         }
 
         Timestamp start = new Timestamp(System.currentTimeMillis());
@@ -90,39 +104,101 @@ public class TestClientApp {
             // Put
             if (isPut) {
                 key = Long.toString(rand.nextLong(ops));
-                client.put(key, value);
+                ///////////////////// Apenas para testar /////////////////////
+                try { Thread.sleep(rand.nextInt(50 - 20) + 20); }         ////
+                catch (InterruptedException e) { e.printStackTrace(); }   ////
+                //////////////////////////////////////////////////////////////
+//                client.put(key, value);
             }
             // Get
             else {
                 key = Long.toString(rand.nextLong(ops));
-                value = client.get(key);
+                ///////////////////// Apenas para testar /////////////////////
+                try { Thread.sleep(rand.nextInt(50 - 20) + 20); }         ////
+                catch (InterruptedException e) { e.printStackTrace(); }   ////
+                //////////////////////////////////////////////////////////////
+//                value = client.get(key);
             }
             time = System.currentTimeMillis() - time;
-            String[] line = new String[] {Long.toString(i), Long.toString(time), key, Boolean.toString(isPut)};
+            String[] line = new String[] {Long.toString(i+1), Long.toString(time), key, Boolean.toString(isPut)};
             data.add(line);
         }
 
         Timestamp end = new Timestamp(System.currentTimeMillis());
+        String testName = NAME + "PutGet";
         CsvExport csvExport = new CsvExport();
-        csvExport.exportDataCsv(data, DIRECTORY, NAME);
+        fileName = csvExport.exportDataCsv(data, DIRECTORY, testName);
         return end.getTime() - start.getTime();
     }
 
     public static void main(String[] args) {
         TestClientApp testClient = new TestClientApp();
-        if (args.length > 1) {
-            String test = args[0];
-            long ops = Long.parseLong(args[1]);
-            float time;
-            if (test.equals("get")) {
-                time = testClient.putWorkload(ops);
+        if (args.length < 2 || args.length > 3) {
+            TestClientApp.errorMessage("Número de argumentos inválido");
+        }
+        else {
+            long ops = -1;
+            int ratio = -1;
+            try {
+                String test = args[0];
+                if (args.length == 2) ops = Long.parseLong(args[1]);
+                else if (args.length == 3) ops = Long.parseLong(args[2]);
+                float time = -1;
+                if (test.equals("-g")) time = testClient.putWorkload(ops);
+                else if (test.equals("-p")) time = testClient.getWorkload(ops);
+                else if (test.equals("-pg") && args.length == 3) {
+                    ratio = Integer.parseInt(args[1]);
+                    if (ratio >= 0 || ratio <= 100) time = testClient.putGetWorkload(ops, ratio);
+                    else errorNumberFormat("ratio");
+                }
+                else TestClientApp.errorMessage("Argumentos inválidos");
+                String dirFileName = "./" + DIRECTORY + "/" + testClient.fileName;
+                TestClientApp.testsEndMessage(time, dirFileName);
             }
-            else if (test.equals("put")) {
-                time = testClient.getWorkload(ops);
+            catch (NumberFormatException e) {
+                if (ops == -1) errorNumberFormat("ops");
+                else if (ratio == -1) errorNumberFormat("ratio");
             }
-            else if (test.equals("getPut") && args.length == 3) {
-                time = testClient.putGetWorkload(ops, Integer.parseInt(args[3]));
+        }
+    }
+
+    private static void errorMessage(String error) {
+        System.out.println(error);
+        System.out.println("  java TestClientApp <tipo de workload> <número de operações>");
+        System.out.println("    Tipo de workload:");
+        System.out.println("      -g : Apenas de Gets");
+        System.out.println("      -p : Apenas de Puts");
+        System.out.println("      -pg <int> : Puts e Gets com parâmetro para percentagem de");
+        System.out.println("                  Puts em relação a Gets, de 0 a 100");
+        System.out.println("    Número de operações:");
+        System.out.println("      <int> : Número de operações a realizar");
+    }
+
+    private static void errorNumberFormat(String type) {
+        if (type.equals("ops")) System.out.println("Número de operações inválido, deve ser um inteiro positivo");
+        else if (type.equals("ratio")) System.out.println("Percentagem de Puts em relação a Gets inválida, deve ser um inteiro entre 0 e 100");
+    }
+
+    private static void testsStartMessage() {
+        System.out.println("Testes a decorrer ...");
+    }
+
+    private static void testsEndMessage(float time, String fileName) {
+        if (time != -1) {
+            String units = " ms";
+            String decimal = "";
+            if (time >= 1000 && time < 60000) {
+                time /= 1000;
+                units = " seg ";
+                decimal = Integer.toString((int) Math.round((time % 1) * 1000)) + " ms";
             }
+            else if (time >= 60000) {
+                time /= 60000;
+                units = " min ";
+                decimal = Integer.toString((int) Math.round((time % 1) * 60)) + " seg";
+            }
+            System.out.println("Testes terminados com sucesso, resultados exportados para " + fileName);
+            System.out.println("Tempo total decorrido: " + ((int) time) + units + decimal);
         }
     }
 }
